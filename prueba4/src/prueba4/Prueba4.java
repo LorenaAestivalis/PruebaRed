63df8d4c03f6b4096de6628aa5443c2d2e59cc4b
LorenaAestivalis <47484011+LorenaAestivalis@users.noreply.github.com>
2021-01-26 18:33:45 -0500

    Add files via upload

--- prueba4/src/prueba4/Prueba4.java
+++ prueba4/src/prueba4/Prueba4.java
@@ -5,97 +5,104 @@
  */
 package prueba4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.FileReader;
+import java.io.FileWriter;
 
-/**
- *
- * @author Lorena Barrios
- */
 public class Prueba4 {
+    /*Este metodo solo especifica los dias que tendra el mes, por lo cual solo devolvera un num**/
+    public static int numDiasDelMes(int mes) {
+        int axio = 2021;
+        int numDias = 0;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static String consumoGetWebService() throws MalformedURLException, IOException {
-
-       /**
-         * Con la clase HttpURLConnection nos ayuda a hacer la conexion con el
-         * WebService definiendo el tipo de metodo que se consumira GET, los
-         * detalles de la cabezera del Request
-         *
-         */
-        URL url = new URL("https://securetransfer.redsis.com/rest/forms/v1/teamGoAny/payload");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setRequestMethod("GET");
-
-        System.out.println("submitFormLink: ");
-        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String output;
-        /**Aqui se lee el response*/
-        StringBuffer response = new StringBuffer();
-        while ((output = in.readLine()) != null) {
-            response.append(output);
-
+        switch (mes) {
+            /**dias que solo tienen 31 dias*/
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                numDias = 31;
+                break;
+            /**dias que solo tienen 30 dias*/
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                numDias = 30;
+                break;
+            /**En este caso es cuando tenemos al año viciesto*/
+            case 2:
+                if ((axio % 4 == 0) && (axio % 100 != 0) || (axio % 400 == 0)) {
+                    numDias = 29;
+                } else {
+                    numDias = 28;
+                }
+                break;
         }
-        in.close();
-        // imprime el response en consola
-        System.out.println("Response:-" + response.toString());
-        String deploydJSON = response.toString();
 
-        System.out.println("Token: " + deploydJSON.subSequence(78, 187));
-
-        return deploydJSON.subSequence(78, 187).toString();
+        return numDias;
     }
 
-    public static void consumePostWebService(String uriWithToken) throws MalformedURLException, IOException {
-        /**
-         * Con la clase HttpURLConnection nos ayuda a hacer la conexion con el
-         * WebService definiendo el tipo de metodo que se consumira POST, los
-         * detalles de la ebzera del Request
-         *
-         */
-        String yourURLStr = java.net.URLEncoder.encode(uriWithToken, "UTF-8");
-        //java.net.URL url = new java.net.URL(yourURLStr);
-        //System.out.println("uriWithToken:-" + uriWithToken);
-        URL url = new URL(uriWithToken);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json; utf-8");
-        con.setRequestProperty("Accept", "application/json");
-        con.setDoOutput(true);
-        String jsonInputString = "{\"nombre\": \"ingresoRedsis\", \"apellido\": \"Qwerty0909$\"}";
-        /*Aqui la cadana con el objeto JSON lo  traemos a un arreglo de bytes para escribirlo en el archivo*/
-        try (OutputStream os = con.getOutputStream()) {
-            byte[] input = jsonInputString.getBytes("utf-8");
-            os.write(input, 0, input.length);
-        }
+    public static void escribirMes(int mes, char diaSemanaDelDiaUno) {
+        String dias = "LMXJVSD";
+        int numDiasMes = numDiasDelMes(mes);
+        int pos = dias.indexOf(Character.toUpperCase(diaSemanaDelDiaUno));
+            //5
+        if (pos != -1) {
+            try {
+                /* aqui defenimos el archivo**/
+                FileWriter fw = new FileWriter("mes" + mes + ".txt");
 
-        /**
-         * las suiguientes lineas se lee el response para imprimirloen consola
-         */
-        try (BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream(), "utf-8"))) {
-            StringBuilder response = new StringBuilder();
-            String responseLine = null;
-            while ((responseLine = br.readLine()) != null) {
-                response.append(responseLine.trim());
+                fw.write("L   M   X   J   V   S   D\r\n");
+                /**rellenamos los espacios  hasta que comienza el primer dia del mes*/
+                for (int i = 0; i < pos * 4; i++) {
+                    fw.write(" ");
+                }
+                /**Escribimos los dias*/
+                for (int i = 1; i <= numDiasMes; i++) {
+
+                    if (i < 10) {
+                        fw.write(i + "   ");
+                    } else {
+                        fw.write(i + "  ");
+                    }
+                    /*aqui nosotros saltamos a la siguiente semana, es decir comenzamos al L**/
+                    pos++;
+                    if (pos > 6) {
+                        fw.write("\r\n");
+                        pos = 0;
+                    }
+                }
+
+                fw.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            System.out.println(response.toString());
+
+        }
+    }
+    /*este metodo simplemente lee el arhvo que previamente se creo**/
+    public static void Calendario(int mes) {
+        try {
+            FileReader fr = new FileReader("mes" + mes + ".txt");
+            int i = fr.read();
+
+            while (i != -1) {
+                System.out.print((char) i);
+                i = fr.read();
+            }
+
+            fr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        /* aqui primero consumiemos el GetWebService  lo cual nos regresara la uri con el token
-    para despues si consumir PostWebService, asi se desplegara el response en consola**/
-        String uri = consumoGetWebService();
-        consumePostWebService(uri);
+    public static void main(String[] args) {
+        escribirMes(3, 'm');
+        Calendario(9);
     }
-
 }
